c78e3a771904368082a9ad894f76c857823da163
Jan Trojanowski <jan.trojanowski@performgroup.com>
2015-03-10 17:24:33 +0100

    removed app.js

--- js/app.js
+++
@@ -1,4 +0,0 @@
-var roomsArray = ['National', 'Lewandowski', 'NextToMama', 'NextToPapa'];
-var mainContent = document.getElementById('content');
-var usersList = ['Janek', 'Grzegorz', 'Rafał', 'Michał'];
-
